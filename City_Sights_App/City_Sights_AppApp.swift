8127f0335ff28dc9d024d26e9f5aed7a429ce81f
Ibrahim Khalikov <starlink050@gmail.com>
2021-06-28 17:05:19 +0500

    Initial Commit

---
+++ City_Sights_App/City_Sights_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_Sights_AppApp.swift
+//  City_Sights_App
+//
+//  Created by cloud_vfx on 28/06/21.
+//
+
+import SwiftUI
+
+@main
+struct City_Sights_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ City_Sights_App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  City_Sights_App
+//
+//  Created by cloud_vfx on 28/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
